1626c2ac0d75105cc4ab1ee19df43e3d91e2396a
Meredith Bloom <31168207+meredithbloom@users.noreply.github.com>
2022-12-08 02:17:02 +0000

    got solution to day 4 part 1 working

--- day-4/day-4.py
+++ day-4/day-4.py
@@ -5,13 +5,42 @@
 # only need to identify those assignment pairs where one range completely contains the other range
 
 
-input1 = open('day4-test-input.txt')
+#input1 = open('day4-test-input.txt')
 
+input1 = open('day-4-input.txt')
 
 pairs = input1.read().split('\n')
 
 #print(pairs)
+redundancy_count = 0
+
+
 
 for pair in pairs:
 	sections = pair.split(',')
-	print(sections)
+	ranges = []
+	for s in sections:
+		try:
+			ranges.append(tuple(map(int, s.split('-'))))
+		except ValueError:
+			break;
+	#print(ranges)
+	if len(ranges) == 0:
+		break;
+	b1, b2 = ranges[0][0], ranges[1][0]
+	#print(b1,b2)
+	t1, t2 = ranges[0][1], ranges[1][1]
+	#print(t1, t2)
+	# if ranges don't touch at all we can move on
+	if (t1 < b2) or (t2 < b1):
+		pass
+		#print('no overlap')
+	elif (b1>=b2) and (t1<=t2):
+		redundancy_count += 1
+		#print(b2,t2, ' contains ', b1,t1)
+	elif (b1<=b2) and (t1>=t2):
+		redundancy_count += 1
+		#print(b1,t1, ' contains ', b2,t2)
+print(redundancy_count)
+
+
